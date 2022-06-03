b754f1b1cd7dfded3207ea7cf831fb020c699a2a
Huan Lin <hellohuanlin@gmail.com>
2022-06-03 16:42:30 -0700

    setup platform view for ui test

--- dev/integration_tests/ios_platform_view_xcuitests/ios/Runner/AppDelegate.m
+++ dev/integration_tests/ios_platform_view_xcuitests/ios/Runner/AppDelegate.m
@@ -1,12 +1,40 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
 
+@interface TextFieldPlatformView: NSObject<FlutterPlatformView>
+@property (nonatomic, strong) UITextField *textField;
+@end
+
+@implementation TextFieldPlatformView
+- (instancetype)init {
+  if ((self = [super init])) {
+    _textField = [[UITextField alloc] init];
+    _textField.text = @"Platform Text Field";
+    _textField.backgroundColor = UIColor.blueColor;
+  }
+  return self;
+}
+
+- (UIView *)view {
+  return self.textField;
+}
+@end
+
+@interface TextFieldPlatformViewFactory: NSObject<FlutterPlatformViewFactory>
+@end
+@implementation TextFieldPlatformViewFactory
+- (NSObject<FlutterPlatformView> *)createWithFrame:(CGRect)frame viewIdentifier:(int64_t)viewId arguments:(id)args {
+  return [[TextFieldPlatformView alloc] init];
+}
+@end
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+  [[self registrarForPlugin:@"flutter"] registerViewFactory:[[TextFieldPlatformViewFactory alloc] init] withId:@"text_field"];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
